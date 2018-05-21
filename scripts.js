c2ee0e840f3c41516cf9dde54c2f0ff89a21cd95
jairoarivas <jairoalbertorivas@gmail.com>
2018-05-21 17:39:40 -0400

    welcome

--- scripts.js
+++ scripts.js
@@ -3,11 +3,11 @@ $('.toggleButton').on('click', function(){
   $(this).toggleClass('clicked');
 });
 
-function removeAfter4seconds(){
+function removeAfter3seconds(){
   var e = document.getElementById('welcomeDiv');
   e.style.display ='none';
   var b = document.getElementById('togButton');
   b.style.display ='block';
 }
 
-setTimeout(removeAfter4seconds,4000);
+setTimeout(removeAfter3seconds,3000);
