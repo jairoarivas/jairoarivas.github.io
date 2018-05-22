749d8bb3584188cff079ed8eb5c2c96857205f64
jairoarivas <jairoalbertorivas@gmail.com>
2018-05-21 20:08:01 -0400

    My name is Jairo added

--- scripts.js
+++ scripts.js
@@ -1,13 +1,25 @@
 // script for adding "clicked" class when toggle menu button is clicked
 $('.toggleButton').on('click', function(){
   $(this).toggleClass('clicked');
+  var e = document.getElementsByClassName('welcome2');
+  if($(this).hasClass('clicked')){
+    e[0].style.display = 'none';
+  }
+  else{
+    e[0].style.display = 'block';
+  }
 });
 
-function removeAfter3seconds(){
+function welcomeTransition(){
   var e = document.getElementById('welcomeDiv');
   e.style.display ='none';
   var b = document.getElementById('togButton');
   b.style.display ='block';
+  var w = document.getElementById('welcomeDiv2');
+  w.style.display ='block';
+  $(w).toggleClass('welcomeOver');
+  var s = document.getElementsByClassName('welcome2');
+  s[0].style.display = 'block';
 }
 
-setTimeout(removeAfter3seconds,3000);
+setTimeout(welcomeTransition,3000);
