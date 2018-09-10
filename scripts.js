28d4b055e94c4731ef1563180d31cd9f06fc79d3
jairoarivas <jairoalbertorivas@gmail.com>
2018-09-10 19:26:29 -0400

    Redoing home page

--- scripts.js
+++ scripts.js
@@ -1,73 +1,77 @@
 // rest of site is a class used to transition between nav menu and page
 var g = document.getElementsByClassName('restOfSite');
 for(var i =0; i < g.length; i++){
-  g[i].style.display = 'none';
+  g[i].style.display = 'flex';
 }
 
-// animating cards when hovering
-$('.cardDes').hover(function(){
-    var x = $(this).prev();
-    x.addClass('animate');
-},function(){
-    var x = $(this).prev();
-    x.removeClass('animate');
-});
-$('.card1Image').hover(function(){
-    var x = $(this);
-    x.addClass('animate');
-},function(){
-    var x = $(this);
-    x.removeClass('animate');
-});
-$('.card2Image').hover(function(){
-    var x = $(this);
-    x.addClass('animate');
-},function(){
-    var x = $(this);
-    x.removeClass('animate');
-});
-$('.card3Image').hover(function(){
-    var x = $(this);
-    x.addClass('animate');
-},function(){
-    var x = $(this);
-    x.removeClass('animate');
-});
+// var r = document.getElementsByClassName('restOfSite');
+// for(var i =0; i < g.length; i++){
+//   r[i].style.display = 'flex';
+// }
+// // animating cards when hovering
+// $('.cardDes').hover(function(){
+//     var x = $(this).prev();
+//     x.addClass('animate');
+// },function(){
+//     var x = $(this).prev();
+//     x.removeClass('animate');
+// });
+// $('.card1Image').hover(function(){
+//     var x = $(this);
+//     x.addClass('animate');
+// },function(){
+//     var x = $(this);
+//     x.removeClass('animate');
+// });
+// $('.card2Image').hover(function(){
+//     var x = $(this);
+//     x.addClass('animate');
+// },function(){
+//     var x = $(this);
+//     x.removeClass('animate');
+// });
+// $('.card3Image').hover(function(){
+//     var x = $(this);
+//     x.addClass('animate');
+// },function(){
+//     var x = $(this);
+//     x.removeClass('animate');
+// });
 
 // script for adding "clicked" class when toggle menu button is clicked
 
 $('.toggleButton').on('click', function(){
-  var e = document.getElementsByClassName('welcome2');
+  // var e = document.getElementsByClassName('welcome2');
   var r = document.getElementsByClassName('restOfSite');
   $(this).toggleClass('clicked');
   if($(this).hasClass('clicked')){
-    e[0].style.display = 'none';
+    // e[0].style.display = 'none';
     for(var i =0; i < r.length; i++){
       r[i].style.display = 'none';
     }
   }
   else{
-    e[0].style.display = 'block';
+    // e[0].style.display = 'block';
     for(var i =0; i < r.length; i++){
       r[i].style.display = 'flex';
     }
   }
 });
 
-function welcomeTransition(){
-  var e = document.getElementById('welcomeDiv');
-  e.style.display ='none';
-  var b = document.getElementById('togButton');
-  b.style.display ='block';
-  var w = document.getElementById('welcomeDiv2');
-  w.style.display ='block';
-  $(w).toggleClass('welcomeOver');
-  var s = document.getElementsByClassName('welcome2');
-  s[0].style.display = 'block';
-  var r = document.getElementsByClassName('restOfSite');
-  for(var i =0; i < g.length; i++){
-    r[i].style.display = 'flex';
-  }
-}
+// function welcomeTransition(){
+//   // var e = document.getElementById('welcomeDiv');
+//   // e.style.display ='none';
+//   var b = document.getElementById('togButton');
+//   b.style.display ='block';
+//   // var w = document.getElementById('welcomeDiv2');
+//   // w.style.display ='block';
+//   // $(w).toggleClass('welcomeOver');
+//   // var s = document.getElementsByClassName('welcome2');
+//   // s[0].style.display = 'block';
+//   var r = document.getElementsByClassName('restOfSite');
+//   for(var i =0; i < g.length; i++){
+//     r[i].style.display = 'flex';
+//   }
+// }
 
-setTimeout(welcomeTransition,3000);
+// setTimeout(welcomeTransition,3000);
